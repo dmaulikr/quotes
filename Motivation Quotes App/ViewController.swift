80bb604401bc4e6788cc17c28871e259079dffb9
Matin Shoghi <matinshoghi@Matins-MacBook-Pro.local>
2017-08-06 23:28:19 +0300

    Initial Commit

---
+++ Motivation Quotes App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Motivation Quotes App
+//
+//  Created by Matin Shoghi on 6/8/17.
+//  Copyright © 2017 Matin Shoghi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
